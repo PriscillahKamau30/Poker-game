cc91685ba776ee61c51a40fe652d98422e4bdcd5
PriscillahKamau30 <priscillahkgathoni>
2026-04-18 12:10:11 +0300

    TH-1 Create Card class

---
+++ Card.py
@@ -0,0 +1,23 @@
+class Card():
+    
+    RANK = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+    SUITE = ['Hearts','Diamonds','Clubs','Spades']
+
+    def __init__(self,suite,rank):
+        
+        if not isinstance(suite,str):
+            raise TypeError('Suite must be a string')
+        if not isinstance(rank,str):
+            raise TypeError('Rank must be a string')
+        
+        if suite not in Card.SUITE:
+            raise ValueError('Suite must be one of the following: ' + ', '.join(Card.SUITE))
+        
+        if rank not in Card.RANK:
+            raise ValueError('Rank must be one of the following: '+ ', '.join(Card.RANK))
+        
+        self.suite = suite
+        self.rank = rank
+        
+    def printCard(self):
+        return self.rank + ' of ' + self.suite
